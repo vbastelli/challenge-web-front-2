1aedfa450677b6f917ca774525b89b40ed5e3c51
Victório <victoriobastelli@gmail.com>
2024-11-03 21:59:52 -0300

    fix

--- src/componentes/Nav/index.jsx
+++ src/componentes/Nav/index.jsx
@@ -40,68 +40,69 @@ const Navbar = () => {
     };
   }, []);
 
-  return (
-    <nav className="navbar">
-      <div className="navbar-container">
+  return (<nav className="navbar">
+    <div className="navbar-container">
+      <div className="navbar-brands">
         <Link className="navbar-brand" to="/">
           <img src="formulaelogo.png" alt="Logo" className="formula-logo" />
         </Link>
-        
         <Link className="navbar-brand" to="/">
           <img src="mahindralogo.png" alt="Second Logo" className="mahindra-logo" />
         </Link>
-
-        <button className="navbar-toggler" type="button" onClick={toggleMenu}>
-          ☰
-        </button>
-
-        <div className={`navbar-links ${isOpen ? 'show' : ''}`}>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/news">Notícias</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/tabela">Tabela de Classificação</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/pilotos">Pilotos</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/quiz">Quiz</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/sobre-nos">Sobre Nós</Link>
-            </li>
-            <li className="nav-item dropdown" ref={raceDataDropdownRef}>
-              <span className="nav-link dropdown-toggle" onClick={toggleRaceDataDropdown}>
-                Dados de Corrida
-              </span>
-              <ul className={`dropdown-menu ${isRaceDataDropdownOpen ? 'show' : ''}`}>
-                <li>
-                  <Link className="dropdown-item" to="/dados">Dados Clima</Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/eventos">Eventos</Link>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item dropdown" ref={accountDropdownRef}>
-              <span className="nav-link dropdown-toggle" onClick={toggleAccountDropdown}>
-                Conta
-              </span>
-              <ul className={`dropdown-menu ${isAccountDropdownOpen ? 'show' : ''}`}>
-                <li>
-                  <Link className="dropdown-item" to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/perfil">Perfil</Link>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </div>
       </div>
-    </nav>
+  
+      <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+        ☰
+      </button>
+  
+      <div className={`navbar-links ${isOpen ? 'show' : ''}`}>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/news">Notícias</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/tabela">Tabela de Classificação</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/pilotos">Pilotos</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/quiz">Quiz</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/sobre-nos">Sobre Nós</Link>
+          </li>
+          <li className="nav-item dropdown" ref={raceDataDropdownRef}>
+            <span className="nav-link dropdown-toggle" onClick={toggleRaceDataDropdown}>
+              Dados de Corrida
+            </span>
+            <ul className={`dropdown-menu ${isRaceDataDropdownOpen ? 'show' : ''}`}>
+              <li>
+                <Link className="dropdown-item" to="/dados">Dados Clima</Link>
+              </li>
+              <li>
+                <Link className="dropdown-item" to="/eventos">Eventos</Link>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown" ref={accountDropdownRef}>
+            <span className="nav-link dropdown-toggle" onClick={toggleAccountDropdown}>
+              Conta
+            </span>
+            <ul className={`dropdown-menu ${isAccountDropdownOpen ? 'show' : ''}`}>
+              <li>
+                <Link className="dropdown-item" to="/login">Login</Link>
+              </li>
+              <li>
+                <Link className="dropdown-item" to="/perfil">Perfil</Link>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
   );
 };
 
